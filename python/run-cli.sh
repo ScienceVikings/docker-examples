0f4407ed2bf34c975f392bf8b4fd655450f194e0
Justin Basinger <justin@sciencevikinglabs.com>
2022-05-18 12:47:31 -0400

    Making docker-compose build before running again

--- python/run-cli.sh
+++ python/run-cli.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "This will give you a prompt and share the files."
 echo "Change hello.py and run python hello.py in the prompt."
+docker-compose build python-cli
 docker-compose run python-cli
--- python/run-with-compose.sh
+++ python/run-with-compose.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "This runs the python service from the docker-compose.yml file."
 echo "It's an easier way to run docker commands."
+docker-compose build
 docker-compose up python
 #https://docs.docker.com/compose/compose-file/compose-file-v3/
 #Running docker-compose up -d <service> will kick it off in the background.
